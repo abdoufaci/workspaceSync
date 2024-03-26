5beb1b86f277ade6424a40b1c47ba0216290dd92
abdoufaci <hello@plupx.com>
2024-03-26 18:41:42 +0100

    layout-side-bar

--- components/side-bar.tsx
+++ components/side-bar.tsx
@@ -18,45 +18,44 @@ function SideBar() {
       onMouseEnter={() => setOpenSideBar(true)}
       onMouseLeave={() => setOpenSideBar(false)}
       className={cn(
-        "border-r overflow-x-hidden flex flex-col items-center gap-x-5 transition-all duration-200 ease-out inset-0",
+        "border-r overflow-x-hidden flex flex-col items-center transition-all duration-300 ease-out",
         openSideBar ? "w-64" : "w-[90px] "
       )}>
       <div
         className={cn(
-          "flex text-[#404040] border-b w-full inset-0",
+          "flex text-[#404040] border-b w-fit -space-x-5 transition-all duration-300 ease-out",
           openSideBar ? "items-center" : "items-center"
         )}>
         <Image
           alt="logo"
           src="/logo.svg"
-          height={50}
-          width={50}
-          className="h-20 w-24 "
+          height={"50"}
+          width={"50"}
+          className="h-20 w-[90px]"
         />
         <h1
           className={cn(
-            "font-semibold text-xl transition-all duration-300 ease-out",
-            openSideBar ? "w-full" : "w-0"
+            "font-semibold transition-all duration-100 ease-out",
+            openSideBar ? "text-xl" : "text-[0px]"
           )}>
           WorkSpaceSync
         </h1>
       </div>
       <div
         className={cn(
-          "space-y-4 flex flex-col w-full  transition-all duration-300 ease-out inset-0",
-          openSideBar ? "items-start p-8" : "justify-center items-center py-8"
+          "space-y-4 flex flex-col w-full justify-center pl-5  transition-all duration-300 ease-out py-8",
+          openSideBar ? "items-start" : "items-start"
         )}>
         {sidebarcontent.map(({ icon: Icon, title, label }) => (
           <Link
             key={label}
             href={label}
             className={cn(
-              "flex items-center p-3 text-secondary-1 w-full inset-0",
+              "flex items-start py-3 text-secondary-1 w-fit px-3 transition-all duration-200 ease-out",
               openSideBar ? "gap-x-3 px-7" : "justify-center",
               pathname === label
-                ? "bg-gradient-to-r from-[#4f5bd5] justify-center to-[#b224ef] rounded-full text-white w-fit bg-blend-color-burn"
-                : "",
-              openSideBar && pathname ? "" : ""
+                ? "bg-gradient-to-r from-[#4f5bd5] justify-center to-[#b224ef] rounded-full text-white  bg-blend-color-burn"
+                : ""
             )}>
             {Icon}
             <h1
