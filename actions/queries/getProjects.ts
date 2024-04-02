27a8eb85350011ef778db125cf1258bec8f1041a
abdoufaci <hello@plupx.com>
2024-04-02 14:57:48 +0100

    project card ui

--- actions/queries/getProjects.ts
+++ actions/queries/getProjects.ts
@@ -2,17 +2,19 @@ import { db } from "@/lib/db";
 
 export const getProjects = async () => {
   const projects = await db.project.findMany({
-      orderBy: {
-        createdAt: "desc",
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      assignedTo: {
+        select: {
+          username: true,
+          imageUrl: true,
+          employeeRole: true,
+        },
       },
-      include: {
-        assignedTo: {
-          select: {
-            imageUrl: true
-          }
-        }
-      }
-    });
+    },
+  });
 
   return projects;
 };
--- app/(main)/my-projects/page.tsx
+++ app/(main)/my-projects/page.tsx
@@ -6,66 +6,66 @@ import Image from "next/image";
 import { Progress } from "@/components/ui/progress";
 import { Badge } from "@/components/ui/badge";
 import Link from "next/link";
+import { AnimatedTooltip } from "@/components/ui/animated-tooltip";
 
 export default async function Page() {
   const projects = await getProjects();
 
   return (
-    <div className="h-[calc(100vh-81px)] w-full p-6 bg-gray-sub-100">
+    <div className="min-h-[calc(100vh-81px)] w-full p-6 bg-gray-sub-100">
       <ProjectsPageHeader />
-      <div className="flex gap-x-4 gap-y-6 flex-wrap mt-4">
-        {projects.map((project) => (
-          <Link key={project.id} href={`my-projects/${project.id}`}>
-            <div className="flex flex-col gap-y-4 px-6 py-10 bg-white rounded-xl w-[32.3%]">
-              <div className="flex items-center gap-x-2 justify-between">
-                <div className="flex items-center gap-x-2">
-                  <Image
-                    alt="project image"
-                    src={project.imageUrl || "/avatar.png"}
-                    height={100}
-                    width={100}
-                    className="h-16 w-16 rounded-md object-cover"
-                  />
-                  <h1 className="font-bold text-2xl">{project.title}</h1>
+      <div className="flex flex-row gap-x-4 gap-y-6 flex-wrap mt-4">
+        {projects.map((project) => {
+          let items = project.assignedTo.map((employee, idx) => ({
+            id: idx,
+            name: employee.username || "workspaceSync employee",
+            image: employee.imageUrl || "/avatar.png",
+            designation: employee.employeeRole || "Chikoour",
+          }));
+          return (
+            <Link
+              key={project.id}
+              href={`my-projects/${project.id}`}
+              className="w-[32.3%]">
+              <div className="flex flex-col gap-y-4 px-6 py-10 bg-white rounded-xl w-full">
+                <div className="flex items-center gap-x-2 justify-between">
+                  <div className="flex items-center gap-x-2">
+                    <Image
+                      alt="project image"
+                      src={project.imageUrl || "/avatar.png"}
+                      height={100}
+                      width={100}
+                      className="h-16 w-16 rounded-md object-cover"
+                    />
+                    <h1 className="font-bold text-2xl">{project.title}</h1>
+                  </div>
+                  <Badge
+                    variant={
+                      project.stat === "inProgress" ? "pending" : project.stat
+                    }>
+                    {project.stat}
+                  </Badge>
                 </div>
-                <Badge
-                  variant={
-                    project.stat === "inProgress" ? "pending" : project.stat
+                <p className="text-sm font-extralight leading-tight">
+                  {project.description}
+                </p>
+                <Progress
+                  value={
+                    project.stat === "completed"
+                      ? 100
+                      : project.stat === "notStarted"
+                      ? 0
+                      : 50
                   }
-                >
-                  {project.stat}
-                </Badge>
+                  className="h-[10px]"
+                />
+                <div className="ml-1 flex items-center gap-2">
+                  <AnimatedTooltip items={items} />
+                </div>
               </div>
-              <p className="text-sm font-extralight leading-tight">
-                {project.description}
-              </p>
-              <Progress
-                value={
-                  project.stat === "completed"
-                    ? 100
-                    : project.stat === "notStarted"
-                    ? 0
-                    : 50
-                }
-                className="h-[10px]"
-              />
-              <div className="ml-1 flex items-center gap-2">
-                {project.assignedTo.slice(0, 4).map((employee, index) => (
-                  <Image
-                    key={index}
-                    src={employee.imageUrl || "/avatar.png"}
-                    alt={`user_${index}`}
-                    width={24}
-                    height={24}
-                    className={`${
-                      index !== 0 && "-ml-4"
-                    } rounded-full object-cover`}
-                  />
-                ))}
-              </div>
-            </div>
-          </Link>
-        ))}
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
